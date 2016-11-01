e0d15091c3a0dea87088a4c36f0a8ba566b1434f
Anderson Chen <anderson_chen@trend.com.tw>
2016-11-01 18:21:32 +0800

    Fix deployment errors and update helloworld/echo with Lambda-proxy style

--- src/echo/index.js
+++ src/echo/index.js
@@ -1,7 +1,16 @@
 import { handlerEnricher } from '../lib/logging'
 
 const handler = (event, context, callback, log) => {
-    callback(null, event)
+    log.info('Echo!')
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'x-custom-header': 'My Header Value',
+        },
+        body: JSON.stringify(event),
+    }
+    callback(null, response)
 }
 
 exports.handler = handlerEnricher(handler)
--- src/helloworld/index.js
+++ src/helloworld/index.js
@@ -1,7 +1,17 @@
 import { handlerEnricher } from '../lib/logging'
 
 const handler = (event, context, callback, log) => {
-    callback(null, { Message: 'Hello World!'})
+    log.info('Hello World!')
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'x-custom-header': 'My Header Value',
+        },
+        body: JSON.stringify({ message: 'Welcome to WestWorld!' }),
+    }
+
+    callback(null, response)
 }
 
 exports.handler = handlerEnricher(handler)
